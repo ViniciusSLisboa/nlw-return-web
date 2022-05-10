05b282123b0da9d49daeeab5f680f22fe6eeab09
vinicius <v.soares.lisboa@gmail.com>
2022-05-10 10:25:06 +0900

    Fazendo uma gracinha...

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export function App() {
   return (
     <div className="bg-black h-screen">
       <Widget />
+      <div className="h-full flex justify-center items-center">
+        <h1>Maria Fernanda, eu estou te observando...</h1>
+      </div>
     </div>
   );
 }
